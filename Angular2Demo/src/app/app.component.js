881369b72cdd6fc590a7c3eb50319c770f76e75a
sk71994 <kumar.shubham71994@gmail.com>
2020-06-21 00:49:18 +0530

    Created and Edited app.component.html file

--- Angular2Demo/src/app/app.component.js
+++ Angular2Demo/src/app/app.component.js
@@ -9,12 +9,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var AppComponent = /** @class */ (function () {
     function AppComponent() {
-        this.name = 'Angular 2!';
+        this.name = 'Angular';
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<h1>Hello {{name}}</h1>",
+            templateUrl: "app/app.component.html"
         })
     ], AppComponent);
     return AppComponent;
